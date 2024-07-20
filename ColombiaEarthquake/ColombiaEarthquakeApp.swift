2e5fe95e4157ba60c524d77f6295ff7255ae0289
miguel <belloingmiguel@gmail.com>
2024-07-19 19:06:11 -0500

    Initial Commit

---
+++ ColombiaEarthquake/ColombiaEarthquakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColombiaEarthquakeApp.swift
+//  ColombiaEarthquake
+//
+//  Created by miguel angel bello garcia on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ColombiaEarthquakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColombiaEarthquake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColombiaEarthquake
+//
+//  Created by miguel angel bello garcia on 19/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
