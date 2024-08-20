3faa37d6dcefbecf794a0d36086f46774d3d4aed
miguel <belloingmiguel@gmail.com>
2024-08-20 15:22:23 -0500

    complete notification by place

---
+++ ColombiaEarthquake/aplication/usecaseCoordinators/BackgroundFetchUseCaseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  BackgroundFetchCoordinator.swift
+//  ColombiaEarthquake
+//
+//  Created by miguel angel bello garcia on 20/08/24.
+//
+
+import Foundation
---
+++ ColombiaEarthquake/domain/usecaseInterfaces/BackgroundFetchUseCaseProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  BackgroundFetchUseCaseProtocol.swift
+//  ColombiaEarthquake
+//
+//  Created by miguel angel bello garcia on 20/08/24.
+//
+
+import Foundation
